2bb12e2cad90d936555d326c4e78bdcd40741120
Renan Garcia <renanrgarcia@gmail.com>
2025-07-30 11:59:15 -0300

    Add component concepts and dynamic data binding notes
    - Created a new markdown file for component concepts, detailing the structure, creation, and usage of components in Angular.
    - Added examples of component class definitions, inline templates, and the use of classes in Angular.
    - Created a new markdown file for dynamic data binding, covering data binding syntax, property binding, attribute binding, and the use of getters and setters.
    - Included examples for event binding and handling user interactions in Angular templates.

---
+++ 2.Essentials/01-starting-project/src/app/user/user.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from './dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css'
+})
+export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get userImage() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  selectUser() {
+    console.log('User selected:', this.selectedUser.name);
+  }
+}
