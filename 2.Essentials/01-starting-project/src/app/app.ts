a7ad52fcc17bf8424e73e831a610b15de0295f10
Renan Garcia <renanrgarcia@gmail.com>
2025-08-01 10:20:05 -0300

    feat: Enhance user component with ID input and emit select event; update app template for user selection

--- 2.Essentials/01-starting-project/src/app/app.ts
+++ 2.Essentials/01-starting-project/src/app/app.ts
@@ -1,15 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header';
-import { User } from "./user/user";
+import { User } from './user/user';
 import { DUMMY_USERS } from './user/dummy-users';
 
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent, User],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('01-starting-project');
   users = DUMMY_USERS;
+
+  onUserSelected(id: string) {
+    console.log('Selected user ID:', id);
+  }
 }
--- 2.Essentials/01-starting-project/src/app/user/user.ts
+++ 2.Essentials/01-starting-project/src/app/user/user.ts
@@ -1,19 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrl: './user.css',
 })
 export class User {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
- 
+  @Output() select = new EventEmitter();
+
   get userImage() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() { }
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
-
