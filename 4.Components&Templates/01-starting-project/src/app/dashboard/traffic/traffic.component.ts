b2d55d2008110658646da64ae3d2d3d48cacc9b2
Renan Garcia <renanrgarcia@gmail.com>
2025-11-21 06:49:58 -0300

    feat: Initialize Angular project with components for dashboard, header, and server status
    - Added package.json with Angular dependencies and scripts
    - Created public assets including favicon and images
    - Implemented app component with dashboard items for server status, traffic, and support tickets
    - Developed dashboard item component for displaying dynamic content
    - Created server status component to show current server status
    - Implemented traffic component to visualize traffic data
    - Added tickets component as a placeholder for support tickets
    - Created header component for navigation
    - Set up main application entry point and index.html
    - Added global styles for layout and components
    - Configured TypeScript settings for the project
    - Documented content projection in Angular

---
+++ 4.Components&Templates/01-starting-project/src/app/dashboard/traffic/traffic.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic',
+  standalone: true,
+  imports: [],
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.css',
+})
+export class TrafficComponent {
+  @Input() dummyTrafficData: { id: string; value: number }[] = [];
+  @Input() maxTraffic: number = 0;
+}
