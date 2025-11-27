31af30eb78d29369e9159aca4aa8aed88e1020d1
Renan Garcia <renanrgarcia@gmail.com>
2025-11-27 08:37:39 -0300

    feat: Refactor dashboard and ticket components; update styles and structure for improved readability and maintainability

--- 4.Components_Templates/01-starting-project/src/app/shared/control/control.component.ts
+++ 4.Components_Templates/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,13 +1,35 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css',
+  styleUrls: ['./control.component.css'],
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; // Discouraged way
+  // @HostListener('click') onClick() {
+  //   console.log('Control clicked!');
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Control clicked!');
+    console.log(this.el);
+  }
 }
